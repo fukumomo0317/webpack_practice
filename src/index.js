724e1b4fec985c4e40514c1fb9be7e9b3a8afca7
fukumomo <momoka8070@gmail.com>
2023-09-03 21:37:37 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// 読み込み
+import my from './modules/my.js';
+
+console.log("webpack!");
+
+// 実行
+my();
---
+++ src/modules/my.js
@@ -0,0 +1,16 @@
+export default () => {
+    console.log('This is module');
+}
+
+
+
+// functionを使わない方法（アロー関数）
+// export default function(){
+//     console.log('This is module');
+// }
+
+
+// javascriptの場合
+// function my(){
+//     console.log('This is module');
+// }
